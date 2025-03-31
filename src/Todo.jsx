56b6664069283da35b497bae4313eaaacc7ece55
Prashant Raut <prashantraut1308@gmail.com>
2025-03-31 13:31:29 +0530

    added Date to todos

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -22,25 +22,53 @@ function Todo() {
   
   const addTodoHandlel = () =>{
     const newtods = [...todos, inputValue]
-  setTodos(newtods);
+    
+    
+    if (!inputValue){
+
+      alert('todo is emppty please add todo ')
+      return;
+    } 
+    if (todos.includes(inputValue)) {
+      alert(`${inputValue} Is Already In Your Todo List ! 😳😳`)
+      return;
+    }
+    
+    setTodos(newtods);
+  setInputValue("")
   
+}
+
+
+const handledelete = (id) =>{
+  // 
+  console.log(id.target );
+   
   
 }
 console.log(todos);
+const now = new Date();
+
+const date = now.toLocaleDateString();
+
 
   return (
-    <div>
+    <div className='bg-gray-900 h-screen'>
         <h1 className='shadow flex justify-center p-2 text-4xl font-bold text-red-500'>
-            Todo 
+            Todo {date}
         </h1>
 
         <div className='flex justify-center p-4 gap-3.5'> 
-        <input type="text" onChange={handleInput} className="border border-amber-300 p-2 px-3 rounded-2xl outline-blue-500"/> <button className=' cursor-pointer bg-red-500 hover:bg-red-400 px-4 py-2 rounded-3xl' onClick={addTodoHandlel}>Add</button>
+        <input type="text" value={inputValue} onChange={handleInput} className="border border-amber-300 text-white p-2 px-3 rounded-2xl outline-blue-500"/> <button className=' cursor-pointer bg-red-500 hover:bg-red-400 px-4 py-2 rounded-3xl' onClick={addTodoHandlel}>Add</button>
         </div>
  {todos.map((todos, key) =>
    (      
 <div key={key} className='flex justify-center mt-3 items-center  '>
-<h1 className='flex justify-center items-center bg-amber-200 w-[300px]  p-2 rounded-2xl'>{todos}</h1><button>❌</button><button>➡️</button>
+<h1 className='flex justify-center items-center bg-amber-200 w-[300px]  p-2 rounded-2xl'>{todos}</h1>
+<button 
+onClick={handledelete}
+>❌</button>
+<button>➡️</button>
     
 </div> )
  )}     
